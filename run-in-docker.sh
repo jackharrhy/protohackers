477d160859e40833ed4d5ef53ab274b8c9adb971
Jack Arthur Harrhy <me@jackharrhy.com>
2022-09-10 11:01:29 -0230

    fixup run in docker script, since we now have mix deps

--- run-in-docker.sh
+++ run-in-docker.sh
@@ -1,1 +1,10 @@
-docker run -it -v $(pwd):/app -p 4040:4040 --rm -w "/app" elixir iex -S mix
+#!/bin/sh
+docker run -it \
+  -v $(pwd):/app \
+  -p 4040:4040 \
+  -p 4050:4050 \
+  --rm \
+  -w "/app" \
+  elixir \
+  /bin/sh -c \
+  "cd /app; mix local.hex --force; mix deps.get; iex -S mix"
